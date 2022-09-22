bf7f0ba2f7c0b2b7d709c62ed62468a24cd5a090
Flávio <flaviofspe@gmail.com>
2022-09-21 22:48:00 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=48):
         self.idade = idade
         self.nome = nome
@@ -8,16 +9,23 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    flavio = Pessoa(nome='Flávio')
-    luiz = Pessoa(flavio, nome='Luiz')
-    print(Pessoa.cumprimentar(luiz))
-    print(id(luiz))
-    print(luiz.cumprimentar())
-    print(luiz.nome)
-    print(luiz.idade)
-    for filho in luiz.filhos:
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
         print(filho.nome)
-    luiz.sobrenome = 'Maia'
-    del luiz.filhos
-    print(luiz.__dict__)
-    print(flavio.__dict__)
+    luciano.sobrenome = 'Maia'
+    del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
